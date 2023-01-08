7cc8567fca6108479938e9ea513e45a022f48de9
Orlando-pt <orlando.macedo15@ua.pt>
2023-01-08 15:36:55 +0000

    adds bash configs

---
+++ bash-configs.sh
@@ -0,0 +1,9 @@
+alias ssh-add-personal-key='eval $(ssh-agent -s) && ssh-add ~/.ssh/personal-key'
+
+idea() {
+  /Applications/IntelliJ\ IDEA.app/Contents/MacOS/idea $1 > /dev/null 2>&1 &
+}
+
+new-vim-tmux() {
+  tmux new "nvim $1"
+}
--- vscode-native-shortcuts.md
+++ vscode-native-shortcuts.md
@@ -2,3 +2,6 @@
 `<C-/>`
 
 Note: To turn off the sound of vscode follow the [link](https://stackoverflow.com/questions/45904944/how-to-turn-off-bell-sound-in-visual-studio-code).
+
+### check buffers
+`<M-p>`
