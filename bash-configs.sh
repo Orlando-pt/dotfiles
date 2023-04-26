2166734790f15db90dfb829e7ee594b347bcd83d
Orlando-pt <orlando.macedo15@ua.pt>
2023-04-26 16:25:55 +0100

    add windows configuration

--- bash-configs.sh
+++ bash-configs.sh
@@ -1,40 +1,51 @@
 alias ssh-personal-key='eval $(ssh-agent -s) && ssh-add ~/.ssh/personal-key'
-alias v='nvim'
 
-alias copy='xsel -ib'
+alias ll='ls -la'
 
-export GIT_EDITOR=nvim
-
-alias gia='git add'
-alias gic='git commit -m'
-alias gip='git push'
-alias gis='git status'
-alias gipl='git pull'
-alias gid='git diff'
-alias gir='git restore'
-alias gif='git fetch'
-alias gich='git checkout'
-alias gib='git branch'
-alias gil='git log'
-alias gim='git merge'
-
-idea() {
-  /Applications/IntelliJ\ IDEA.app/Contents/MacOS/idea $1 > /dev/null 2>&1 &
-}
-
-# tmux
-new-vim-tmux() {
-  tmux new "nvim $1"
-}
-
-tnew() {
-  tmux new
-}
+alias gad='git add'
+alias gco='git commit -m'
+alias gpu='git push'
+alias gst='git status'
+alias gpl='git pull'
+alias gdi='git diff'
+alias gre='git restore'
+alias gfe='git fetch'
+alias gch='git checkout'
+alias gbr='git branch'
+alias glo='git log'
+alias gme='git merge'
 
 # git branch delete with pattern
-gibd() {
+gbdel() {
   git branch | grep $1 | xargs git branch -$2
 }
 
-# add go bin to path
-export PATH="$PATH:$HOME/go/bin/"
+# only do this steps on macOS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "macOS detected"
+  # add go bin to path
+  export PATH="$PATH:$HOME/go/bin/"
+
+  # tmux
+  new-vim-tmux() {
+    tmux new "nvim $1"
+  }
+
+  tnew() {
+    tmux new
+  }
+
+  idea() {
+    /Applications/IntelliJ\ IDEA.app/Contents/MacOS/idea $1 > /dev/null 2>&1 &
+  }
+
+  alias v='nvim'
+  export GIT_EDITOR=nvim
+fi
+
+# only do this steps on windows ughhhh
+if [[ "$OSTYPE" == "Windows_NT"* ]]; then
+  echo "Windows detected"
+
+  export GIT_EDITOR=vim
+fi
