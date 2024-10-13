1d4b0c6f456675cb6991b8b9b618a1c1ddfe6d9e
Orlando Macedo <orlando.macedo@tui.com>
2024-10-13 19:26:36 +0100

    add links

---
+++ links.sh
@@ -0,0 +1,7 @@
+ln vscode-settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln intellij-keymap.xml ~/Library/Application Support/JetBrains/IntelliJIdea2023.3/options/mac/keymap.xml
+ln keybindings.json ~/Library/Application Support/Code/User/keybindings.json
+ln .ideavimrc ~/.ideavimrc
+ln .zshrc ~/.zshrc
+ln .vimrc ~/.vimrc
+ln .tmux.conf ~/.tmux.conf
