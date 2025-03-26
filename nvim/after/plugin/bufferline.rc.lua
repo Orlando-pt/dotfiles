0d833442cc755b083008339afae0e5ba5cda148d
Orlando Macedo <orlando.macedo@tui.com>
2025-03-26 22:17:21 +0000

    remove error on bufferline

--- nvim/after/plugin/bufferline.rc.lua
+++ nvim/after/plugin/bufferline.rc.lua
@@ -1,22 +1,22 @@
 -- vip.opt.termguicolors = true
 
 local bufferline = require("bufferline")
-bufferline.setup {
-  options = {
-    numbers = "ordinal",
-    diagnostics = "nvim_lsp",
-    diagnostics_indicator = function(_, _, diagnostics_dict, _)
-      local s = " "
-      for e, n in pairs(diagnostics_dict) do
-        local sym = e == "error" and " "
-            or (e == "warning" and " " or "")
-        s = s .. n .. sym
-      end
-      return s
-    end,
-    show_buffer_close_icons = false,
-  }
-}
+-- bufferline.setup {
+--   options = {
+--     numbers = "ordinal",
+--     diagnostics = "nvim_lsp",
+--     diagnostics_indicator = function(_, _, diagnostics_dict, _)
+--       local s = " "
+--       for e, n in pairs(diagnostics_dict) do
+--         local sym = e == "error" and " "
+--             or (e == "warning" and " " or "")
+--         s = s .. n .. sym
+--       end
+--       return s
+--     end,
+--     show_buffer_close_icons = false,
+--   }
+-- }
 
 for i = 1, 9 do
   vim.keymap.set("n", "<leader>" .. i, function()
