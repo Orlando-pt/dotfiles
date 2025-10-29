6f6e2e6afab9d9ee6d681d56de4a6b3f6c5adc43
Orlando Macedo <orkdgg57@gmail.com>
2025-10-29 12:49:22 +0000

    migrate lspconfig

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -1,6 +1,3 @@
-local status, nvim_lsp = pcall(require, "lspconfig")
-if (not status) then return end
-
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
@@ -22,19 +19,20 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
 end
 
-nvim_lsp.ts_ls.setup {
+vim.lsp.config('ts_ls', {
   on_attach = on_attach,
   filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
   cmd = { "typescript-language-server", "--stdio" },
   --    capabilities = capabilities
-}
+})
 
-nvim_lsp.gopls.setup {
+vim.lsp.config('gopls', {
   on_attach = on_attach,
   cmd = { "gopls", "serve" },
-}
+})
 
-nvim_lsp.pylsp.setup {
+vim.lsp.config('pylsp', {
+
   on_attach = on_attach,
   settings = {
     pylsp = {
@@ -46,9 +44,9 @@ nvim_lsp.pylsp.setup {
       }
     }
   }
-}
+})
 
-nvim_lsp.lua_ls.setup {
+vim.lsp.config('lua_ls', {
   on_attach = on_attach,
   settings = {
     Lua = {
@@ -56,7 +54,6 @@ nvim_lsp.lua_ls.setup {
         -- Get the language server to recognize the `vim` global
         globals = { 'vim' },
       },
-
       workspace = {
         -- Make the server aware of Neovim runtime files
         library = vim.api.nvim_get_runtime_file("", true),
@@ -64,14 +61,14 @@ nvim_lsp.lua_ls.setup {
       },
     },
   },
-}
+})
 
 --Enable (broadcasting) snippet capability for completion
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
-nvim_lsp.jsonls.setup {
+vim.lsp.config('jsonls', {
   capabilities = capabilities,
-}
+})
 
 -- Diagnostic symbols in the sign column (gutter)
 local signs = { Error = " ", Warn = " ", Hint = "󰈈 ", Info = " " }
